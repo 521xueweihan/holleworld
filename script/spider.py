1a1c95eb8980fa26601e03b6968ebadb9bd85c95
xueweihan <595666367@qq.com>
2016-06-20 22:17:27 +0800

    写一个聚集电影资源的脚本

--- script/spider.py
+++ script/spider.py
@@ -190,4 +190,12 @@ def main():
                 flag += 1
 
 if __name__ == '__main__':
-    IOLoop().run_sync(main)
+    # IOLoop().run_sync(main)
+    url = 'https://movie.douban.com/top250'
+    s = Spider(url)
+    response_obj = s.get()
+    soup = BeautifulSoup(response_obj.body, 'html.parser')
+    items = soup.find_all("div", class_="item")
+    for i, fi_itmes in enumerate(items):
+        print i
+        print fi_itmes
